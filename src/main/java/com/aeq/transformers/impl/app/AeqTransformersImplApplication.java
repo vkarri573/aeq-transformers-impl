0afad952b4e3984f9332fd6bf9fbdec4334de983
venukarri <venu_kvks@yahoo.com>
2021-03-10 00:23:46 +0530

    Add initial code base for transformers project

---
+++ src/main/java/com/aeq/transformers/impl/app/AeqTransformersImplApplication.java
@@ -0,0 +1,13 @@
+package com.aeq.transformers.impl.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeqTransformersImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeqTransformersImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aeq/transformers/impl/app/AeqTransformersImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeq.transformers.impl.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeqTransformersImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
